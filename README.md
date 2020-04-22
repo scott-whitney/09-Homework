06710212c0693f4b57d12c4df892c8b12e968d70
Minroe <scott.whitney334@gmail.com>
2020-04-22 09:39:56 -0700

    first heroku commit

---
+++ README.md
@@ -0,0 +1,32 @@
+
+*Tue, 21 Apr 2020 00:32:52 GMT*
+
+# Description
+*This is a notes saver website using a server and a JSON file to act as a database.*
+
+# table of contents:
+1. Description
+1. Installation
+1. Usage
+1. Contributors
+1. Additional Info
+
+# Intstallation
+npm i in root folder then use nodemon ./server.js
+
+# Usage
+this website is just a simple note taker to help you remember what to do with your day
+
+### GitHub UserName
+*scott-whitney*
+### Contributors
+**none**
+
+# Additional Info
+*none*
+##scott.whitney334##
+[GitHub](https://api.github.com/users/scott-whitney)
+![Github Avatar](https://avatars1.githubusercontent.com/u/60905905?v=4)
+*I'm an aspiring full stack developer still currently in training.*
+null
+null
---
+++ server.js
@@ -0,0 +1,99 @@
+// require dependencies
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const databasePath = path.join(__dirname, "/db/db.json");
+
+const uuidv4 = require('uuid/v4');
+const database = require("./db/db.json");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+app.use(express.static("public"));
+// // notes.html
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+// return all saved notes in database as JSON
+app.get("/api/notes", function(req, res) {
+    res.json(database);
+  });
+
+app.post("/api/notes", function(req, res) {
+    fs.readFile("./db/db.json", function read(err, data) {
+        if (err) {
+            throw err;
+        }
+        let content = data;
+        let readContent = JSON.parse(content)
+
+       
+
+        req.body.id = uuidv4();
+
+
+        readContent.push(req.body)
+        // pushing the new note into the hold note.
+
+       newNotes = JSON.stringify(readContent)
+
+        fs.writeFile("./db/db.json", newNotes, function (err) {
+
+           if (err) throw err;
+           console.log("Note successfully added")
+           console.log(data)
+
+ 
+       })
+
+    });
+
+    res.sendFile(path.join(__dirname, "./db/db.json"))
+})
+
+app.delete ("/api/notes/:id", function(req, res) {
+
+    fs.readFile("./db/db.json", function read(err, data) {
+        if (err){
+            throw err;
+        }
+        let currentDelete = req.params.id
+
+        console.log(currentDelete)
+        let content = data
+
+        console.log(JSON.parse(content))
+
+        let readContent = JSON.parse(content)
+        var index = readContent.findIndex(p => p.id == currentDelete)
+
+        console.log(index)
+
+        readContent.splice(index, 1)
+ 
+        
+
+        console.log(readContent)
+        newNotes = JSON.stringify(readContent)
+        fs.writeFile("./db/db.json", newNotes, function (err) {
+
+            if (err) throw err;
+            console.log("Note successfully delete")
+            console.log(data)
+ 
+          res.sendFile(path.join(__dirname, "./db/db.json"))
+        })
+
+    })
+
+})
+
+
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
